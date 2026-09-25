6ac2035fdb1902a6ebb924f45f9d9b10b5c6a4f1
pos <pgousdal@users.noreply.github.com>
2026-09-25 03:46:55 +0200

    M1: add reconnect lifecycle and graceful shutdown

--- cmd/engo/main.go
+++ cmd/engo/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Ploos-AS/Engo/internal/config"
 	"github.com/Ploos-AS/Engo/internal/irc"
@@ -11,29 +15,68 @@ import (
 
 func main() {
 	cfg := config.FromEnv()
+	if err := cfg.Validate(); err != nil {
+		fatal(err)
+	}
 
 	if err := script.RunFile(cfg.Script); err != nil {
 		fatal(err)
 	}
-
 	if cfg.Server == "" {
 		return
 	}
 
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	delay := cfg.ReconnectMin
+	for {
+		err := runIRC(ctx, cfg)
+		if ctx.Err() != nil {
+			return
+		}
+		fmt.Fprintf(os.Stderr, "engo: IRC session ended: %v; reconnecting in %s\n", err, delay)
+
+		timer := time.NewTimer(delay)
+		select {
+		case <-ctx.Done():
+			timer.Stop()
+			return
+		case <-timer.C:
+		}
+
+		delay *= 2
+		if delay > cfg.ReconnectMax {
+			delay = cfg.ReconnectMax
+		}
+	}
+}
+
+func runIRC(ctx context.Context, cfg config.Config) error {
 	client, err := irc.Dial(irc.Config{
-		Server:   cfg.Server,
-		Nick:     cfg.Nick,
-		User:     cfg.User,
-		RealName: cfg.RealName,
-		TLS:      cfg.TLS,
+		Server:       cfg.Server,
+		Nick:         cfg.Nick,
+		User:         cfg.User,
+		RealName:     cfg.RealName,
+		TLS:          cfg.TLS,
+		SASLUsername: cfg.SASLUsername,
+		SASLPassword: cfg.SASLPassword,
 	})
 	if err != nil {
-		fatal(err)
+		return err
 	}
 	defer client.Close()
 
-	if err := client.Run(); err != nil {
-		fatal(err)
+	done := make(chan error, 1)
+	go func() { done <- client.Run() }()
+
+	select {
+	case <-ctx.Done():
+		_ = client.Close()
+		<-done
+		return ctx.Err()
+	case err := <-done:
+		return err
 	}
 }
 

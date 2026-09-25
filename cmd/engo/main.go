ac5f73f03a77342971081ec09a28d7cf88bb4232
pos <pgousdal@users.noreply.github.com>
2026-09-25 03:43:31 +0200

    M0: wire config IRC and Tengo

--- cmd/engo/main.go
+++ cmd/engo/main.go
@@ -4,12 +4,40 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/Ploos-AS/Engo/internal/config"
+	"github.com/Ploos-AS/Engo/internal/irc"
 	"github.com/Ploos-AS/Engo/internal/script"
 )
 
 func main() {
-	if err := script.RunFile("scripts/examples/hello.tengo"); err != nil {
-		fmt.Fprintln(os.Stderr, "engo:", err)
-		os.Exit(1)
+	cfg := config.FromEnv()
+
+	if err := script.RunFile(cfg.Script); err != nil {
+		fatal(err)
 	}
+
+	if cfg.Server == "" {
+		return
+	}
+
+	client, err := irc.Dial(irc.Config{
+		Server:   cfg.Server,
+		Nick:     cfg.Nick,
+		User:     cfg.User,
+		RealName: cfg.RealName,
+		TLS:      cfg.TLS,
+	})
+	if err != nil {
+		fatal(err)
+	}
+	defer client.Close()
+
+	if err := client.Run(); err != nil {
+		fatal(err)
+	}
+}
+
+func fatal(err error) {
+	fmt.Fprintln(os.Stderr, "engo:", err)
+	os.Exit(1)
 }

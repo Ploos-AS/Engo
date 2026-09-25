5cff81564deae9c38f8a36fa04bc1873e1711355
pos <pgousdal@users.noreply.github.com>
2026-09-25 11:44:19 +0200

    Test reconnect backoff reset

---
+++ cmd/engo/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestShouldResetBackoff(t *testing.T){
+	threshold:=2*time.Minute
+	if shouldResetBackoff(30*time.Second,threshold){t.Fatal("short session should not reset backoff")}
+	if !shouldResetBackoff(threshold,threshold){t.Fatal("session at threshold should reset backoff")}
+	if !shouldResetBackoff(10*time.Minute,threshold){t.Fatal("stable session should reset backoff")}
+}

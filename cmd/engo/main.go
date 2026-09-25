47c23d94c4d132e4f2bdf465e7d5cfef88684b1f
pos <pgousdal@users.noreply.github.com>
2026-09-25 03:52:11 +0200

    M3: add script reload signal

--- cmd/engo/main.go
+++ cmd/engo/main.go
@@ -16,75 +16,62 @@ import (
 
 func main() {
 	cfg := config.FromEnv()
-	if err := cfg.Validate(); err != nil {
-		fatal(err)
-	}
-
+	if err := cfg.Validate(); err != nil { fatal(err) }
 	if cfg.Server == "" {
-		if err := script.RunFile(cfg.Script); err != nil {
-			fatal(err)
-		}
+		if err := script.RunFile(cfg.Script); err != nil { fatal(err) }
 		return
 	}
-
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
-
 	delay := cfg.ReconnectMin
 	for {
 		err := runIRC(ctx, cfg)
-		if ctx.Err() != nil {
-			return
-		}
+		if ctx.Err() != nil { return }
 		fmt.Fprintf(os.Stderr, "engo: IRC session ended: %v; reconnecting in %s\n", err, delay)
-
 		timer := time.NewTimer(delay)
 		select {
-		case <-ctx.Done():
-			timer.Stop()
-			return
+		case <-ctx.Done(): timer.Stop(); return
 		case <-timer.C:
 		}
-
 		delay *= 2
-		if delay > cfg.ReconnectMax {
-			delay = cfg.ReconnectMax
-		}
+		if delay > cfg.ReconnectMax { delay = cfg.ReconnectMax }
 	}
 }
 
 func runIRC(ctx context.Context, cfg config.Config) error {
 	client, err := irc.Dial(irc.Config{
-		Server:       cfg.Server,
-		Nick:         cfg.Nick,
-		User:         cfg.User,
-		RealName:     cfg.RealName,
-		TLS:          cfg.TLS,
-		SASLUsername: cfg.SASLUsername,
-		SASLPassword: cfg.SASLPassword,
+		Server: cfg.Server, Nick: cfg.Nick, User: cfg.User, RealName: cfg.RealName,
+		TLS: cfg.TLS, SASLUsername: cfg.SASLUsername, SASLPassword: cfg.SASLPassword,
 	})
-	if err != nil {
-		return err
-	}
+	if err != nil { return err }
 	defer client.Close()
 
 	b := bot.New(client)
 	rt := script.New(cfg.Script, b)
-	if err := rt.Load(); err != nil {
-		return err
-	}
+	if err := rt.Load(); err != nil { return err }
 	client.OnMessage(b.Handle)
 
+	reload := make(chan os.Signal, 1)
+	signal.Notify(reload, syscall.SIGHUP)
+	defer signal.Stop(reload)
 	done := make(chan error, 1)
 	go func() { done <- client.Run() }()
 
-	select {
-	case <-ctx.Done():
-		_ = client.Close()
-		<-done
-		return ctx.Err()
-	case err := <-done:
-		return err
+	for {
+		select {
+		case <-ctx.Done():
+			_ = client.Close()
+			<-done
+			return ctx.Err()
+		case err := <-done:
+			return err
+		case <-reload:
+			if err := rt.Reload(); err != nil {
+				fmt.Fprintf(os.Stderr, "engo: script reload rejected; previous version remains active: %v\n", err)
+			} else {
+				fmt.Fprintln(os.Stderr, "engo: script reloaded")
+			}
+		}
 	}
 }
 
